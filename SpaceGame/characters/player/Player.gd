2ca31de72efb4acf027b3c130add137c46f912e6
AaronFacoline <apfacoline@tutanota.com>
2022-08-12 20:31:48 +0200

    added simple animation

--- SpaceGame/characters/player/Player.gd
+++ SpaceGame/characters/player/Player.gd
@@ -9,12 +9,19 @@ var acceleration = 5
 var deceleration = 2
 var maxSpeed = 250
 
+var sprite1 = preload("res://assets/player.png")
+var sprite2 = preload("res://assets/playermove.png")
+onready var mySprite = get_node("Sprite")
+
+
 
 func _unhandled_input(event):
 	if(event.is_action_pressed("move")):
 		moving = true
+		mySprite.set_texture(sprite2)
 	if(event.is_action_released("move")):
 		moving = false
+		mySprite.set_texture(sprite1)
 
 func _physics_process(delta):
 	destination = get_global_mouse_position()
