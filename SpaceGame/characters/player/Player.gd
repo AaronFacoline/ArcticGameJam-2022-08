16f4c955aed7fa176b5f2c0b4cf8a242f4181671
AaronFacoline <apfacoline@tutanota.com>
2022-08-12 16:39:14 +0200

    player basic movement

--- SpaceGame/characters/player/Player.gd
+++ SpaceGame/characters/player/Player.gd
@@ -1,19 +1,27 @@
 extends KinematicBody2D
 
+var inputVector : Vector2
+var velocity : Vector2
+
+var moving = false
+var destination = Vector2()
+
+export (int) var acceleration = 200
+
+func _unhandled_input(event):
+	if(event.is_action_pressed("move")):
+		moving = true
+		destination = get_global_mouse_position()
+	if(event.is_action_released("move")):
+		moving = false
+func _physics_process(delta):
+	if(moving):
+		velocity = position.direction_to(get_global_mouse_position()) * acceleration
+	else:
+		velocity = velocity * 0
+	
+	move_and_slide(velocity)
+
+
 func _process(delta):
 	look_at(get_global_mouse_position())
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
